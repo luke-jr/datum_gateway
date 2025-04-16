d5b226e1197d6b205d5263423f907109d5405c91
Luke Dashjr <luke-jr+git@utopios.org>
2025-04-16 20:33:24 +0000

    Add ENABLE_API CMake option that can be turned off to build w/o libmicrohttpd

--- src/datum_conf.c
+++ src/datum_conf.c
@@ -344,9 +344,16 @@ int datum_read_config(const char *conffile) {
 		}
 	}
 	
+#ifndef ENABLE_API
+	if (datum_config.api_listen_port) {
+		DLOG_WARN("API is enabled in configuration, but this build was compiled without API support");
+	}
+#else
 	if (datum_config.api_modify_conf) {
 		datum_config.config_json = config;
-	} else {
+	} else
+#endif
+	{
 		json_decref(config);
 	}
 	
@@ -387,6 +394,7 @@ int datum_read_config(const char *conffile) {
 		return 0;
 	}
 	
+#ifdef ENABLE_API
 	datum_config.api_admin_password_len = strlen(datum_config.api_admin_password);
 	if (datum_config.api_admin_password_len) {
 		static const char hash_tag[] = "DATUM Anti-CSRF Token";
@@ -400,6 +408,7 @@ int datum_read_config(const char *conffile) {
 		my_sha256(hash, data, data_sz);
 		hash2hex(hash, datum_config.api_csrf_token);
 	}
+#endif
 	
 	if (datum_config.stratum_v1_max_threads > MAX_THREADS) {
 		DLOG_FATAL("Maximum threads must be less than %d.", MAX_THREADS);
