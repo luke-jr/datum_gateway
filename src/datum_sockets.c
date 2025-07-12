ddba6dc22830b9a845e80ee6aac10083c41e0b25
Luke Dashjr <luke-jr+git@utopios.org>
2025-07-12 16:37:02 +0000

    sockets: Combine PROXY mode checks

--- src/datum_sockets.c
+++ src/datum_sockets.c
@@ -270,31 +270,29 @@ void *datum_threadpool_thread(void *arg) {
 						
 						while (end_line != NULL) {
 							*end_line = 0; // null terminate the line
-							if (datum_config.stratum_v1_trust_proxy != -1) {
-								if (my->client_data[cidx].proxy_line_read != -1) {
-									if (strncmp(start_line, "PROXY ", 6) == 0) {
-										my->client_data[cidx].proxy_line_read += 1;
-										if (my->client_data[cidx].proxy_line_read <= datum_config.stratum_v1_trust_proxy) {
-											char src_ip[DATUM_MAX_IP_LEN + 1];
-											int matched = sscanf(start_line, "PROXY TCP4 %15s", src_ip);
-											if (matched != 1) {
-												matched = sscanf(start_line, "PROXY TCP6 %45s", src_ip);
-											}
-											if (matched == 1 && src_ip[0] != 0) {
-												DLOG_DEBUG("New proxy IP detected: %s on TID: %d, CID: %d", src_ip, my->thread_id, my->client_data[cidx].cid);
-												strcpy(my->client_data[cidx].rem_host, src_ip);
-											}
-											else {
-												DLOG_DEBUG("PROXY line present but no valid IP found, keeping original source IP: %s on TID: %d, CID: %d", my->client_data[cidx].rem_host, my->thread_id, my->client_data[cidx].cid);
-											}
+							if (datum_config.stratum_v1_trust_proxy != -1 && my->client_data[cidx].proxy_line_read != -1) {
+								if (strncmp(start_line, "PROXY ", 6) == 0) {
+									my->client_data[cidx].proxy_line_read += 1;
+									if (my->client_data[cidx].proxy_line_read <= datum_config.stratum_v1_trust_proxy) {
+										char src_ip[DATUM_MAX_IP_LEN + 1];
+										int matched = sscanf(start_line, "PROXY TCP4 %15s", src_ip);
+										if (matched != 1) {
+											matched = sscanf(start_line, "PROXY TCP6 %45s", src_ip);
 										}
-										start_line = end_line + 1;
-										end_line = strchr(start_line, '\n');
-										continue;
-									} else {
-										DLOG_DEBUG("Received non-PROXY line from client %d/%d", my->thread_id, my->client_data[cidx].cid);
-										my->client_data[cidx].proxy_line_read = -1;
+										if (matched == 1 && src_ip[0] != 0) {
+											DLOG_DEBUG("New proxy IP detected: %s on TID: %d, CID: %d", src_ip, my->thread_id, my->client_data[cidx].cid);
+											strcpy(my->client_data[cidx].rem_host, src_ip);
+										}
+										else {
+											DLOG_DEBUG("PROXY line present but no valid IP found, keeping original source IP: %s on TID: %d, CID: %d", my->client_data[cidx].rem_host, my->thread_id, my->client_data[cidx].cid);
+										}
 									}
+									start_line = end_line + 1;
+									end_line = strchr(start_line, '\n');
+									continue;
+								} else {
+									DLOG_DEBUG("Received non-PROXY line from client %d/%d", my->thread_id, my->client_data[cidx].cid);
+									my->client_data[cidx].proxy_line_read = -1;
 								}
 							}
 							// this function can not be NULL
