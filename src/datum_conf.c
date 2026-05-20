2ea8dac158bd6e5c51a7eba93891cd60b301b58c
Luke Dashjr <luke-jr+git@utopios.org>
2026-05-20 11:22:46 +0000

    Bugfix: conf: Error if string array is configured with too many items

--- src/datum_conf.c
+++ src/datum_conf.c
@@ -239,11 +239,10 @@ int datum_config_parse_value(const T_DATUM_CONFIG_ITEM *c, json_t *item) {
 			
 			json_array_foreach(item, index, value) {
 				if (!json_is_string(value)) return -1;
-				if (i < (DATUM_CONFIG_MAX_ARRAY_ENTRIES-1)) {
-					strncpy(((char (*)[1024])c->ptr)[i], json_string_value(value), c->max_string_len-1);
-					((char (*)[1024])c->ptr)[i][c->max_string_len-1] = 0;
-					i++;
-				}
+				if (i >= DATUM_CONFIG_MAX_ARRAY_ENTRIES - 1) return -3;
+				strncpy(((char (*)[1024])c->ptr)[i], json_string_value(value), c->max_string_len-1);
+				((char (*)[1024])c->ptr)[i][c->max_string_len-1] = 0;
+				i++;
 			}
 			((char (*)[1024])c->ptr)[i][0] = 0;
 			return 1;
@@ -291,6 +290,10 @@ int datum_read_config(const char *conffile) {
 		// item might be valid
 		j = datum_config_parse_value(&datum_config_options[i], item);
 		if (j != 1) {
+			if (j == -3) {
+				DLOG_ERROR("Configuration option %s.%s exceeds maximum list size of %u", datum_config_options[i].category, datum_config_options[i].name, (unsigned int)(DATUM_CONFIG_MAX_ARRAY_ENTRIES - 1));
+				return -1;
+			}
 			DLOG_ERROR("Could not parse configuration option %s.%s.  Type should be %s", datum_config_options[i].category, datum_config_options[i].name, (datum_config_options[i].var_type<DATUM_CONF_TYPES)?datum_conf_var_type_text[datum_config_options[i].var_type]:"UNKNOWN");
 		}
 	}
